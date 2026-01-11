920d0de3d941c2b4dc51edf41393f061e7912090
Lain-Ego0 <2302109068@qq.com>
2026-01-11 23:15:00 +0800

    Update

---
+++ 2.Software/BRS-260111-2315-V2.1.2/User/main.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f4xx.h"
+#include "bsp_clkconfig.h"
+#include "bsp_debug_usart.h"
+#include "GO_M8010_6.h"
+#include "bsp_usart_dma.h"
+#include "pid.h"
+#include "bsp_general_tim.h"
+#include "bsp_key.h" 
+#include "motor_control.h" 
+#include "remote.h"
+
+#endif 
+
