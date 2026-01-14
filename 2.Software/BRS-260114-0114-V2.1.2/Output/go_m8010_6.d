e0809c23b5424094d4a5be5cbc0f02562a0c3a4f
Lain-Ego0 <2302109068@qq.com>
2026-01-14 22:11:49 +0800

    Update

--- 2.Software/BRS-260114-0114-V2.1.2/Output/go_m8010_6.d
+++
@@ -1,6 +0,0 @@
-..\..\output\go_m8010_6.o: ..\..\User\APP\GO-M8010-6\GO_M8010_6.c
-..\..\output\go_m8010_6.o: ..\..\User\APP\GO-M8010-6\GO_M8010_6.h
-..\..\output\go_m8010_6.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\..\output\go_m8010_6.o: ..\..\User\APP\GO-M8010-6\ris_protocol.h
-..\..\output\go_m8010_6.o: ..\..\User\APP\GO-M8010-6\crc_ccitt.h
-..\..\output\go_m8010_6.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
